aa38f33fb0af2dd01738fc44ffe6b5daae1814ec
Juan Luis <juanlupriv24@gmail.com>
2025-02-05 16:14:52 +0100

    Login

--- app/tabs/index.tsx
+++ app/tabs/index.tsx
@@ -1,11 +1,14 @@
-import { View, Text } from 'react-native'
+import { View, Text, TextInput } from 'react-native'
 import React from 'react'
 import { GlobalStyles } from '@/theme/GlobalStyles'
 
 const ScreenPrincipal = () => {
   return (
     <View style={GlobalStyles.containerCentrado}>
-      <Text style= {GlobalStyles.titulo}>Screen Principal</Text>
+      <Text style= {GlobalStyles.titulo}>LOG IN</Text>
+      <TextInput style= {GlobalStyles.estiloinput} placeholder='Nombre de usuario'></TextInput>
+      <TextInput style= {GlobalStyles.estiloinput} placeholder='Contraseña'></TextInput>
+
     </View>
   )
 }
--- theme/GlobalStyles.tsx
+++ theme/GlobalStyles.tsx
@@ -7,6 +7,19 @@ export const GlobalStyles = StyleSheet.create({
         alignItems: 'center'
     },
     titulo: {
-        fontSize: 22
+        fontSize: 25,
+        fontFamily: 'Roboto',
+        fontWeight: 'bold'
+    },
+    estiloinput:{
+        margin: 15,
+        borderColor: '#55555',
+        borderWidth: 3,
+        borderRadius: 15,
+        padding: 20,
+        fontFamily: 'Roboto',
+        color: '#55555',
+        width: '95%',
+        backgroundColor: '#fff'
     }
 })
